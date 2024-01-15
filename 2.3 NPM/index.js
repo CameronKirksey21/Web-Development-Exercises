9a02e4c081d0f39ae713f15812e1f2945a0c4890
CameronKirksey21 <Cameron_kirksey21@yahoo.com>
2024-01-15 12:15:34 -0600

    Finished QR Project

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// let generateName = require('sillyname');
+// import generateName from 'sillyname';
+// let sillyName = generateName();
+import superheroes from 'superheroes';
+let superheroName = superheroes.random();
+
+// console.log(`My Name is ${sillyName}.`);
+console.log(`I am ${superheroName}!`);
