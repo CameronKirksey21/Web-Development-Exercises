69c3098388cd15aff9af6a06c84fa314ee9fc253
CameronKirksey21 <Cameron_kirksey21@yahoo.com>
2024-02-21 10:09:07 -0600

    Finished Part 2 Tracker

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -18,33 +18,38 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', async (req, res) => {
+async function checkVisisted() {
   const result = await db.query('SELECT country_code FROM visited_countries');
+
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(result.rows);
+  return countries;
+}
+
+// GET home page
+app.get('/', async (req, res) => {
+  const countries = await checkVisisted();
   res.render('index.ejs', { countries: countries, total: countries.length });
-  db.end();
 });
 
+//INSERT new country
 app.post('/add', async (req, res) => {
   const input = req.body['country'];
 
   const result = await db.query(
-    'SELECT country_code from countries WHERE country_name = $1',
+    'SELECT country_code FROM countries WHERE country_name = $1',
     [input]
   );
 
-  if (result.row.length !== 0) {
+  if (result.rows.length !== 0) {
     const data = result.rows[0];
     const countryCode = data.country_code;
 
-    await db.query('INSERT INTO visites_countries (country_code) VALUES ($1)', [
+    await db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [
       countryCode,
     ]);
-
     res.redirect('/');
   }
 });
